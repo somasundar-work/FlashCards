96dca5af49c0b8741d4d8cc9eece8f6f8e44b9eb
Somasundar <somausundar.work@outlook.com>
2025-01-02 19:51:16 +0530

    Route & Route Icon Changes

--- FlashCards.UI/src/app/app.routes.ts
+++ FlashCards.UI/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './features/home/home.component';
 import { DecksComponent } from './features/decks/decks.component';
 import { ReviewsComponent } from './features/reviews/reviews.component';
 import { SettingsComponent } from './features/settings/settings.component';
+import { CardsComponent } from './features/cards/cards.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'decks', component: DecksComponent },
+  { path: 'cards', component: CardsComponent },
   { path: 'reviews', component: ReviewsComponent },
   { path: 'settings', component: SettingsComponent },
   { path: '**', redirectTo: 'error/not-found', pathMatch: 'full' },
